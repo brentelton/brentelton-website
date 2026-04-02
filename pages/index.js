a8e162b6303161edda1e11d2cea2e88478d7cfe4
brentelton <brentelton.sep@gmail.com>
2026-04-02 11:43:25 +1000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
 
             <div className="heroImageCard">
               <img
-                src="/Sunrise event .JPG"
+                src="/hero.JPG"
                 alt="Brent Elton at sunrise"
                 className="heroImage"
               />
